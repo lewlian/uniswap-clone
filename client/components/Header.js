2ad6c4fb6630a7b7e08737775a6c3f1fa0be1bb2
lewlian <seanlewtengsiong@gmail.com>
2022-02-18 00:22:00 +0800

    completed main portion for BC

--- client/components/Header.js
+++ client/components/Header.js
@@ -25,28 +25,18 @@ const style = {
 const Header = () => {
   const [selectedNav, setSelectedNav] = useState('swap')
   const { currentAccount, connectWallet } = useContext(TransactionContext)
+  const [userName, setUserName] = useState()
+
+  useEffect(() => {
+    if (!currentAccount) {
+      return
+    }else{
+      setUserName(`${currentAccount.slice(0, 7)}...${currentAccount.slice(35)}`)
+    }
+    
+  }, [currentAccount])
 
   console.log({ currentAccount, connectWallet })
-  // useEffect(() => {
-  //   if (currentAccount) {
-  //     ;(async () => {
-  //       const query = `
-  //       *[_type=="users" && _id == "${currentAccount}"] {
-  //         userName,
-  //       }
-  //       `
-  //       const clientRes = await client.fetch(query)
-
-  //       if (!(clientRes[0].userName == 'Unnamed')) {
-  //         setUserName(clientRes[0].userName)
-  //       } else {
-  //         setUserName(
-  //           `${currentAccount.slice(0, 7)}...${currentAccount.slice(35)}`
-  //         )
-  //       }
-  //     })()
-  //   }
-  // }, [currentAccount])
 
   return (
     <div className={style.wrapper}>
@@ -103,7 +93,7 @@ const Header = () => {
 
         {currentAccount ? (
           <div className={`${style.button} ${style.buttonPadding}`}>
-            <div className={style.buttonTextContainer}> 0x88....5c </div>
+            <div className={style.buttonTextContainer}> {userName} </div>
           </div>
         ) : (
           <div
