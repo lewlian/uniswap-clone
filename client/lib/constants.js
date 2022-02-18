f336514bb7fe546ff34d64ed72ef307ee48302f0
lewlian <seanlewtengsiong@gmail.com>
2022-02-18 15:39:48 +0800

    deployed to rinkeby and updated CA

--- client/lib/constants.js
+++ client/lib/constants.js
@@ -1,4 +1,4 @@
 import abi from './Transactions.json'
 
 export const contractABI = abi.abi
-export const contractAddress = ''
+export const contractAddress = '0x0228Ac6eB9D3dcB795e0a21A53980Ec04d0BA5cc'
